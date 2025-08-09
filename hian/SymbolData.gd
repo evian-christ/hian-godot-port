57d43409f4fb63f2c07a20297cfdbea1995f4058
flflvm97 <thelastorcas@gmail.com>
2025-08-10 00:03:48 +0100

    Refactor: Adjust SymbolData properties

--- hian/SymbolData.gd
+++ hian/SymbolData.gd
@@ -2,14 +2,14 @@
 class_name SymbolData
 extends Resource
 
-@export var id: String = ""
+@export var id: int = 0
+@export var name: String = ""
 @export_range(0, 10) var level: int = 0
 
 # 심볼의 타입을 정의하는 플래그 (여러 개 선택 가능)
 @export_flags("Terrain", "Food")
 var types: int = 0
 @export var texture: Texture2D
-@export var base_food: int = 0
 
 # 심볼에 대한 설명
 @export_multiline var description: String = ""
